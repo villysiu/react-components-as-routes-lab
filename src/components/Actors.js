fc66dd5673d388174fc37160776f7d5d2b9891d8
villysiu <villysiu@gmail.com>
2021-07-28 15:04:20 -0700

    commit tree 9d7d6ba056255755e471fb937a671c898fca65aa

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,11 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      { actors.map((actor, i) => <div key={i}>
+                                    <p>name: {actor.name} </p>
+                                    movies: <ul>{actor.movies.map((movie, i) => <li key={i}>{movie}</li>)}</ul>
+                                    </div> )}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,11 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      { directors.map((director, i) => <div key={i}>
+                                    <p>name: {director.name} </p>
+                                    movies: <ul>{director.movies.map((movie, i) => <li key={i}>{movie}</li>)}</ul>
+                                    </div> )}
     </div>
   );
 }
